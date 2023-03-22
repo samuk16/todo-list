235b63b3d110747acea44fb1153fde1a8823ba35
Samuk <marcelolamas19@gmail.com>
2023-03-21 21:44:03 -0300

    Add function for priority popup and array with popup elements

---
+++ src/todos.js
@@ -0,0 +1,253 @@
+import createElementsDom from './domCreation.js';
+
+
+const arrPopUpTodo = [
+
+    {
+        elementType: 'div',
+        attributes: {class:'containerPopUpNewTodo'},
+        appendChild: 'body',
+
+    },
+
+    //  childs containerPopUpNewTodo
+
+    {
+        elementType: 'div',
+        attributes: {class:'btnClosePopUp'},
+        innerHTML: '<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4 12L12 4M4 4L12 12" stroke="#E6E1E5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>',
+        appendChild: '.containerPopUpNewTodo',
+    },
+
+    {
+        elementType: 'div',
+        attributes: {class:'containerName'},
+        appendChild: '.containerPopUpNewTodo',
+
+    },
+
+    {
+        elementType: 'div',
+        attributes: {class:'containerDescription'},
+        appendChild: '.containerPopUpNewTodo',
+
+    },
+
+    {
+        elementType: 'div',
+        attributes: {class:'containerDate'},
+        appendChild: '.containerPopUpNewTodo',
+
+    },
+
+    {
+        elementType: 'div',
+        attributes: {class:'btnCreateTodo'},
+        innerHTML: '<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.00033 2.66666V13.3333M13.3337 7.99999L2.66699 7.99999" stroke="#E6E1E5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>',
+        appendChild: '.containerPopUpNewTodo',
+    },
+
+    //  childs containerName
+
+    {
+        elementType: 'p',
+        attributes: {class:'titleNewTodoName'},
+        innerText: 'Name',
+        appendChild: '.containerName',
+
+    },
+
+    {
+        elementType: 'div',
+        attributes: {class:'containerTextAndPriority'},
+        appendChild: '.containerName',
+
+    },
+
+    {
+        elementType: 'div',
+        attributes: {class:'containerTextTodoName'},
+        innerHTML: '<input class ="input name" type="text" name="nameTodo">',
+        appendChild: '.containerTextAndPriority',
+
+    },
+
+    {
+        elementType: 'div',
+        attributes: {class:'containerPriority'},
+        innerHTML: '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M19 9L12 16L5 9" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>',
+        appendChild: '.containerTextAndPriority',
+
+    },
+
+    //  child containerPriority
+
+    
+
+    // {
+    //     elementType: 'option',
+    //     attributes: {value:"op1"},
+    //     innerHTML: '<svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="6" cy="6" r="5.5" fill="#DB3D3D"/></svg>',
+    //     appendChild: '.dropDown',
+
+    // },
+    
+    // {
+    //     elementType: 'option',
+    //     attributes: {value:"op2"},
+    //     innerHTML: '<svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="6" cy="6" r="5.5" fill="#DB763D"/></svg>',
+    //     appendChild: '.dropDown',
+
+    // },
+
+    // {
+    //     elementType: 'option',
+    //     attributes: {value:"op3"},
+    //     innerHTML: '<svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="6" cy="6" r="5.5" fill="#3D99DB"/></svg>',
+    //     appendChild: '.dropDown',
+
+    // },
+    
+
+    //  childs containerDescription
+
+    {
+        elementType: 'p',
+        attributes: {class:'titleNewTodoDescription'},
+        innerText: 'Description',
+        appendChild: '.containerDescription',
+
+    },
+
+    // {
+    //     elementType: 'div',
+    //     attributes: {class:'containerTextTodoDescription'},
+    //     innerHTML: '<input class ="input description" type="text" name="descriptionTodo">',
+    //     appendChild: '.containerDescription',
+
+    // },
+    {
+        elementType: 'div',
+        attributes: {class:'containerTextTodoDescription'},
+        innerHTML: '<textarea class="description" cols="60">',
+        appendChild: '.containerDescription',
+
+    },
+
+    //  childs containerDate
+
+
+
+    {
+        elementType: 'div',
+        attributes: {class:'containerInputDate'},
+        innerHTML: '<input class ="input date" type="date" name="dateTodo">',
+        appendChild: '.containerDate',
+
+    },
+    
+    
+
+    // child containerinputText
+
+    // {
+    //     elementType: 'div',
+    //     attributes: {class:'containerInputColor'},
+    //     innerHTML: '<input type="color" class="inputColor">',
+    //     appendChild: '.containerInputText',
+
+    // },
+
+
+];
+
+const arrDropDown = [
+
+    {
+        elementType: 'div',
+        attributes: {class:'dropDown'},
+        appendChild: '.containerPriority',
+
+    },
+
+    //  childs dropDown
+
+
+    {
+        elementType: 'div',
+        attributes: {class:'DdOp1 itemDd'},
+        innerHTML: '<svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="6" cy="6" r="5.5" fill="#DB3D3D"/></svg>',
+        appendChild: '.dropDown',
+
+    },
+    {
+        elementType: 'div',
+        attributes: {class:'DdOp2 itemDd'},
+        innerHTML: '<svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="6" cy="6" r="5.5" fill="#DB763D"/></svg>',
+        appendChild: '.dropDown',
+
+    },
+    {
+        elementType: 'div',
+        attributes: {class:'DdOp3 itemDd'},
+        innerHTML: '<svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="6" cy="6" r="5.5" fill="#3D99DB"/></svg>',
+        appendChild: '.dropDown',
+
+    },
+
+]
+
+function domElements(arr) {
+
+    arr.forEach(elementObject => {
+        
+        createElementsDom(elementObject.elementType,elementObject.attributes,elementObject.innerHTML,elementObject.innerText,document.querySelector(elementObject.appendChild));
+        
+    });
+   
+}  
+
+
+function popUpTodo() {
+    const btnPopUpTodo = document.querySelector('.btnNewTodo');
+
+    btnPopUpTodo.addEventListener('click', () => {
+
+        domElements(arrPopUpTodo);
+        addTodo();
+        popUpPriority();
+    })
+}
+
+function popUpPriority() {
+    
+    const containerPriority = document.querySelector('.containerPriority');
+
+    containerPriority.addEventListener('click', () => {
+
+        domElements(arrDropDown);
+
+    })
+
+}
+
+function addTodo() {
+    const btnCreateTodo = document.querySelector('.btnCreateTodo')
+    const inputNameTodo = document.querySelector('.name')
+    const inputDateTodo = document.querySelector('.date')
+    const inputDescriptionTodo = document.querySelector('.description')
+
+    btnCreateTodo.addEventListener('click', () => {
+
+        console.log(inputNameTodo.value);
+        console.log(inputDateTodo.value);
+        console.log(inputDescriptionTodo.value);
+
+    })
+
+
+}
+
+
+
+export {popUpTodo};
