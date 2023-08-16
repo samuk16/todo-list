3c979f56f0c86b21c2974e16eb8c7ceadf53a1e0
Samuk <marcelolamas19@gmail.com>
2023-08-16 14:54:06 -0300

    Refactor mobile menu creation logic

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import './style.css'
 import createElementsDom from './domCreation.js';
 
-import {createPopUpNewProject} from './projects.js';
+import {createPopUpNewProject,menuMobile,projects} from './projects.js';
+
+import {EventManager} from './pubSub.js';
+import {popUpTodo,defaultTodo,showTodayMobile,showWeekMobile} from './todos.js';
+import {init} from './ui.js';
+
 
 
 const arrElementsHome = [
@@ -78,8 +83,14 @@ const arrElementsHome = [
 
     {
         elementType: 'div',
-        attributes: {class:'containerTodo'},
+        attributes: {class:'outer'},
         appendChild: '.containerTodoCenter',
+    },
+
+    {
+        elementType: 'div',
+        attributes: {class:'containerTodo'},
+        appendChild: '.outer',
     },
 
     
@@ -91,6 +102,8 @@ const arrElementsHome = [
         innerHTML: '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 4V20M20 12L4 12" stroke="#E6E1E5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>',
         appendChild: '.containerTodo',
     },
+
+
 
     //  childs containerTodoRight
 
@@ -110,11 +123,11 @@ const arrElementsHome = [
     // childs containerProjects
 
     
-    {
-        elementType: 'div',
-        attributes: {class:'itemProject'},
-        appendChild: '.containerProjects',
-    },
+    // {
+    //     elementType: 'div',
+    //     attributes: {class:'itemProject'},
+    //     appendChild: '.containerProjects',
+    // },
 
     // containerTodayAndWeek
 
@@ -131,55 +144,6 @@ const arrElementsHome = [
         appendChild: '.containerTodayAndWeek',
     },
 
-    //  childs containerTodo
-
-    {
-        elementType: 'div',
-        attributes: {class:'itemTodo1'},
-        appendChild: '.containerTodo',
-    },
-
-    //  childs itemTodo1
-
-    {
-        elementType: 'div',
-        attributes: {class:'svgTodo'},
-        innerHTML: '<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="10" cy="10" r="9" stroke="#25A7B9" stroke-width="2"/></svg>',
-        appendChild: '.itemTodo1',
-    },
-
-    {
-        elementType: 'p',
-        attributes: {class:'pTodo'},
-        innerText: 'eat',
-        appendChild: '.itemTodo1',
-    },
-
-    //  childs containerTodoDay
-
-    // {
-    //     elementType: 'div',
-    //     attributes: {class:'itemTodoDay1'},
-    //     appendChild: '.containerTodoDay',
-    // },
-
-    // {
-    //     elementType: 'div',
-    //     attributes: {class:'svgTodo'},
-    //     innerHTML: '<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="10" cy="10" r="9" stroke="#25A7B9" stroke-width="2"/></svg>',
-    //     appendChild: '.itemTodoDay1',
-    // },
-
-    // {
-    //     elementType: 'p',
-    //     attributes: {class:'pTodoDay'},
-    //     innerText: 'eat',
-    //     appendChild: '.itemTodoDay1',
-    // },
-
-
-
-
     
 ];
 
@@ -193,7 +157,87 @@ function domElements(arr) {
    
 }  
 
+const arrContainerLeftPhone = [
 
+    {
+        elementType: 'div',
+        attributes: {class:'containerLeftMobile'},
+        appendChild: 'body',
+    },
+
+    // child containerLeftMobile
+
+    {
+        elementType: 'div',
+        attributes: {class:'containerProjectsM'},
+        appendChild: '.containerLeftMobile',
+    },
+
+    {
+        elementType: 'div',
+        attributes: {class:'containerTodayM'},
+        appendChild: '.containerLeftMobile',
+    },
+
+    {
+        elementType: 'div',
+        attributes: {class:'containerWeekM'},
+        appendChild: '.containerLeftMobile',
+    },
+
+    // childs containerProjectsM
+
+    
+    {
+        elementType: 'div',
+        attributes: {class:'containerSvgFolder'},
+        innerHTML: '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path class="pathSvgProjects" d="M3 7V17C3 18.1046 3.89543 19 5 19H19C20.1046 19 21 18.1046 21 17V9C21 7.89543 20.1046 7 19 7H13L11 5H5C3.89543 5 3 5.89543 3 7Z" stroke="#E6E1E5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>',
+        appendChild: '.containerProjectsM',
+    },
+
+    {
+        elementType: 'p',
+        attributes: {class:'titleProjectsM'},
+        innerText:'Projects',
+        appendChild: '.containerProjectsM',
+    },
+
+    // childs containerTodayM
+
+    
+    {
+        elementType: 'div',
+        attributes: {class:'containerSvgToday'},
+        innerHTML: '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8 7V3M16 7V3M7 11H17M5 21H19C20.1046 21 21 20.1046 21 19V7C21 5.89543 20.1046 5 19 5H5C3.89543 5 3 5.89543 3 7V19C3 20.1046 3.89543 21 5 21Z" stroke="#E6E1E5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/><rect x="6" y="10" width="12" height="2" fill="#4A4458"/><rect x="6" y="10" width="3" height="2" rx="1" fill="#D9D9D9"/></svg>' ,
+        appendChild: '.containerTodayM',
+    },
+
+    {
+        elementType: 'p',
+        attributes: {class:'titleTodayM'},
+        innerText:'Today',
+        appendChild: '.containerTodayM',
+    },
+    // childs containerProjects
+
+    
+    {
+        elementType: 'div',
+        attributes: {class:'containerSvgWeek'},
+        innerHTML:  '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8 7V3M16 7V3M7 11H17M5 21H19C20.1046 21 21 20.1046 21 19V7C21 5.89543 20.1046 5 19 5H5C3.89543 5 3 5.89543 3 7V19C3 20.1046 3.89543 21 5 21Z" stroke="#E6E1E5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>' ,
+        appendChild: '.containerWeekM',
+    },
+
+    {
+        elementType: 'p',
+        attributes: {class:'titleWeekM'},
+        innerText:'Week',
+        appendChild: '.containerWeekM',
+    },
+
+
+
+]
 
 
 
@@ -202,4 +246,73 @@ function domElements(arr) {
 
 
 domElements(arrElementsHome);
+init();
 createPopUpNewProject();
+// onot()
+popUpTodo();
+
+function resizeWindow() {
+    
+
+    createMobileMenuIfScreenSizeMatches()
+
+    window.addEventListener('resize', () => {
+
+        createMobileMenuIfScreenSizeMatches()
+        
+    
+    } )
+
+}
+
+function createMobileMenuIfScreenSizeMatches() {
+    
+    let screenWidthF = window.screen.availWidth;
+       
+    if (screenWidthF <= 648) {
+        // console.log('menor a 648px');
+        let containerLeftMobile = document.querySelector('.containerLeftMobile');
+
+        if (!containerLeftMobile) {
+            
+            domElements(arrContainerLeftPhone)     
+            menuMobile();   
+            showTodayMobile();    
+            showWeekMobile();
+
+            const containerTodosTodayAndWeek = document.querySelector('.containerTodosTodayAndWeek');
+
+            if (containerTodosTodayAndWeek) {
+             
+                EventManager.emit('fadeOutAndShrink', containerTodosTodayAndWeek)
+
+                setTimeout(() => {
+                    EventManager.emit('deleteElement', containerTodosTodayAndWeek)
+                }, 120);   
+            }
+        }
+    }else if(screenWidthF >= 649){
+        // console.log('mayor a 648');
+        let containerLeftMobile = document.querySelector('.containerLeftMobile');
+
+        if (containerLeftMobile) {
+
+            EventManager.emit('deleteElement', containerLeftMobile);
+
+        }
+
+        const containerProjects = document.querySelector('.containerProjects');
+        // console.log(containerProjects.firstChild);
+
+        if (!containerProjects.firstChild) {
+            EventManager.emit('renderProjects',projects)
+            EventManager.emit('transitionGhostEntryProjects','.itemProject')
+        }
+
+    }
+
+}
+
+
+
+resizeWindow()
